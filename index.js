aedc2b3a8a0651feef4643c36cb028c1752786f9
JosephS <JRySocarras@gmail.com>
2018-09-13 21:57:01 -0500

    tinkering

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-const model = tf.loadModel('https://storage.cloud.google.com/sentiment-analysis-55b/model.json');
+
 
 x_test = np.array(['I am happy to see you'])
 X_test_indices = sentences_to_indices(x_test, word_to_index, maxLen)
 model.predict(X_test_indices)
+
+
+async function myFirstTfjs() {
+    const model = tf.loadModel('https://storage.cloud.google.com/sentiment-analysis-55b/model.json');
+    
+    const xs = tf.tensor2d([-1, 0, 1, 2, 3, 4], [6, 1]);
+    const ys = tf.tensor2d([-3, -1, 1, 3, 5, 7], [6, 1]);
+    
+    await model.fit(xs, ys, {epochs: 250});
+    
+    document.getElementById('micro_out_div').innerText +=
+    model.predict(tf.tensor2d([20], [1, 1]));
+}
+
+myFirstTfjs();
