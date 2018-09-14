13520637e3a3db7ab59afc90166325b57a6c6f0d
Joseph Socarras <37901304+jsocarras@users.noreply.github.com>
2018-09-13 21:05:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const model = await tf.loadModel('https://storage.cloud.google.com/sentiment-analysis-55b/model.json');
+const model = tf.loadModel('https://storage.cloud.google.com/sentiment-analysis-55b/model.json');
 
 x_test = np.array(['I am happy to see you'])
 X_test_indices = sentences_to_indices(x_test, word_to_index, maxLen)
