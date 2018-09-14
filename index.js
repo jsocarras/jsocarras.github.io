845f0873e38e1d743cfa63380724905f142841a9
Joseph Socarras <37901304+jsocarras@users.noreply.github.com>
2018-09-13 19:44:04 -0500

    Create index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+import * as tf from '@tensorflow/tfjs';
+const model = await tf.loadModel('https://storage.cloud.google.com/sentiment-analysis-55b/model.json');
+
+x_test = np.array(['I am happy to see you'])
+X_test_indices = sentences_to_indices(x_test, word_to_index, maxLen)
+model.predict(X_test_indices)
